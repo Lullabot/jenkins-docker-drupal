8f48dc3a29bfc17e9993237008289bebc7f40b4a
Andrew Berry <deviantintegral@gmail.com>
2019-03-20 10:33:43 -0400

    Fix not installing composer correctly

--- Dockerfile.php
+++ Dockerfile.php
@@ -11,13 +11,11 @@ ENV DEBIAN_FRONTEND=noninteractive
 
 # composer needs git and unzip
 RUN apt-get update \
-  && apt-get install -y git unzip
+  && apt-get install -y git unzip wget
 
 # Install composer globally.
-RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-RUN php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-RUN php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-RUN php -r "unlink('composer-setup.php');"
+COPY install-composer.sh install-composer.sh
+RUN ./install-composer.sh
 
 # Add a Jenkins user with the same UID and GID as the Jenkins container itself.
 # Otherwise, permissions will be wrong in the Jenkins workspace (which exists
